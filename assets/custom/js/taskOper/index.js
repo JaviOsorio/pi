a771aa4976520883f95e108a8cceed832b495934
JaviOsorio <javi.osorio.caro@gmail.com>
2024-10-22 15:34:48 -0500

    Update index.js

--- assets/custom/js/taskOper/index.js
+++ assets/custom/js/taskOper/index.js
@@ -73,9 +73,9 @@ document.addEventListener("DOMContentLoaded", async () => {
       $("#detailTaskModal").modal("show");
     } else if (event.target.matches(".btn-scale")) {
       if (parseInt(data.quantity) > 18000) {
-        socket.emit("port", "COM1");
+        socket.emit("port", "COM4");
       } else {
-        socket.emit("port", "COM2");
+        socket.emit("port", "COM3");
       }
       $progresBar.setAttribute("aria-valuemin", 0);
       $progresBar.setAttribute("aria-valuemax", data.quantity);
